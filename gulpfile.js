989f98f48fb6f849baac74bd303f6d81c45ae46e
Jesper Lindstrøm Nielsen <me@jstroem.com>
2017-11-12 18:08:18 +0100

    Fixed buildsystem to add ';' in the end of the file created

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,23 @@
 var gulp = require('gulp');
-var browserify = require('gulp-browserify');
+var browserify = require('browserify');
 var karma = require('karma').server;
 var coveralls = require('gulp-coveralls');
 var run = require('run-sequence');
 var watch = require('gulp-watch');
 var shell = require('gulp-shell');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var gutil = require('gulp-util');
+var rename = require('gulp-rename');
 
 var conf = {
     'js': 'src/**/*.js',
-    'dest': 'dist/'
-}
+    'src': 'src/',
+    'dest': 'dist/',
+    'file': 'ng-input-currency.js'
+};
 
 gulp.task('coveralls', function () {
   gulp.src('coverage/**/lcov.info').pipe(coveralls());
@@ -19,7 +27,7 @@ gulp.task('test', function (done) {
   karma.start({
     configFile: __dirname + '/karma.conf.js'
   }, function (exitCode) {
-    if (exitCode == 0)
+    if (exitCode === 0)
       done();
     else
       process.exit(exitCode);
@@ -32,7 +40,22 @@ gulp.task('publish', shell.task([
 ]));
 
 gulp.task('build:src', function() {
-  return gulp.src(conf.js).pipe(browserify()).pipe(gulp.dest(conf.dest));
+  var b = browserify({
+    entries: conf.src + conf.file,
+    debug: true
+  });
+
+  return b.bundle()
+    .pipe(source(conf.file))
+    .pipe(buffer())
+    .pipe(gulp.dest(conf.dest))
+    .pipe(sourcemaps.init({loadMaps: true}))
+        // Add transformation tasks to the pipeline here.
+        .pipe(uglify())
+        .on('error', gutil.log)
+    .pipe(rename({ extname: '.min.js' }))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(conf.dest));
 });
 
 gulp.task('build', function(){
@@ -51,6 +74,6 @@ gulp.task('watch:build', function() {
   watch(conf.js, function(){
     gulp.start('build:src');
   });
-})
+});
 
 gulp.task('watch', ['watch:build','watch:test']);
